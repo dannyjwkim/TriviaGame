af966de73fa9c7be84943d9fab50be307c607d9f
Daniel Kim <dannyjwkim@Daniels-MacBook-Pro.local>
2017-03-04 01:17:19 -0500

    WIP: Trivia Game Basics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,101 @@
-var loss = 0;
-var win = 0;
-var cardValue = Math.floor(Math.random() * (102)+ 19)
-var cardArray = [];
-var adder = 0;
+$( document ).ready(function() {
 
-$(document).ready(function(){
-  function cardFunction() {
-    for (var i = 0; i < 4; i++){
-    var card = Math.floor(Math.random() * 12) + 1 ;
-    cardArray.push(card);
+var timeRemaining = 0;
+  var questionObject;
+  var timer;
+  var rightAnswers = [];
+  var wrongAnswers = [];
+  var unanswered = [];
+  var remainingQuestions = [ {
+      q: "what is the color of the moon?",
+      answer: [ "Gray", "Blue", "Red", "Green" ],
+      correct: "Gray"
+    }, {
+      q: "where does batman live?",
+      answer: [ "Gotham", "New York", "LA", "Atlanta" ],
+      correct: "Gotham"
+    }, {
+      q: "what is 2 + 2?",
+      answer: [ "4", "7", "8", "12" ],
+      correct: "4"
+    }, {
+      q: "what is the microsoft logo?",
+      answer: [ "window", "f", "mermaid", "triangle" ],
+      correct: "window"
+    },
+
+
+    {
+      q: "what is the shape of a wheel?",
+      answer: [ "circle", "square", "hexagon", "rectangle" ],
+      correct: "circle"
+    }
+
+  ];
+
+  function incrementTimer() {
+    timer = setTimeout( function () {
+      $( '#timer' ).text( timeRemaining );
+      if ( timeRemaining <= 0 ) {
+        //unanswered
+        unanswered.push( questionObject );
+        alert( 'Time is UP' );
+        askQuestion();
+      } else {
+        timeRemaining = timeRemaining - 1;
+        incrementTimer();
+      }
+
+    }, 1000 );
   }
-};
 
-cardFunction();
-$('.targetScore').html(cardValue);
-
-$('.pic1').on("click", function(){
-  var card1Value = (cardArray[0]);
-  adder = adder + card1Value;
-  $('.score').html(adder);
-  checkIt();
-});
-
-$('.pic2').on("click", function(){
-  var card2Value = (cardArray[1]);
-  adder = adder + card2Value;
-  $('.score').html(adder);
-  checkIt();
-});
-
-$('.pic3').on("click", function(){
-  var card4Value = (cardArray[2]);
-  adder = adder + card4Value;
-  $('.score').html(adder);
-  checkIt();
-});
-
-$('.pic4').on("click", function(){
-  var card4Value = (cardArray[3]);
-  adder = adder + card4Value;
-  $('.score').html(adder);
-  checkIt();
-});
-
-function checkIt(){
-  if (cardValue === adder) {
-    alert("You win! Play again!");
-    win = win + 1;
-    $("#wins").html(win);
-    reset();
+  function startTimer() {
+    clearTimeout( timer );
+    timeRemaining = 10; // in seconds
+    incrementTimer();
   }
-  else if (cardValue < adder) {
-    alert("You lose! Try again!");
-    loss = loss + 1;
-    $("#losses").html(loss);
-    reset();
-  }
-};
 
-function reset(){
-  cardArray.length = 0;
-  cardFunction();
-  cardValue = Math.floor(Math.random() * (102)+ 19)
-  $('.targetScore').html(cardValue);
-  adder = 0;
-  $('.score').html(adder);
-}
+  function askQuestion() {
+    if ( remainingQuestions.length <= 0 ) {
+      // you done
+      clearTimeout(timer);
+      alert (" you got correct: " + rightAnswers.length);
+      alert (" you got wrong: " + wrongAnswers.length);
+      alert (" you got unanswered: " + unanswered.length);
+    } else {
+      startTimer();
+      $( '#containerForChoiceOptions' ).html( "" );
+      questionObject = remainingQuestions.pop();
+      //var answer = prompt( question.q );
+      // if ( answer === question.answer )
+      //   rightAnswers.push( question );
+      // else
+      //   wrongAnswers.push( question );
+      //   
+      var choices = questionObject.answer;
+      $( '#asked-question' ).html( questionObject.q );
+      for ( var i = 0; i < choices.length; i++ ) {
+        var choice = $( '<div>' );
+        choice.text( choices[ i ] );
+        choice.attr( 'id', "choice-" + i );
+        choice.attr( 'index', i );
+        $( '#containerForChoiceOptions' ).append( choice );
+
+        choice.click( function () {
+          //alert("I GUESSED "+ this.innerHTML);
+          if ( this.innerHTML === questionObject.correct ) {
+            alert( "YAY" );
+            rightAnswers.push( questionObject );
+            askQuestion();
+          } else {
+            alert( "boo!" );
+            wrongAnswers.push( questionObject );
+            askQuestion();
+          }
+        } );
+      }
+    }
+  }
+  askQuestion();
 
 });
