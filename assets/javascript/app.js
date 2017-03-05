1dbeb1d5b144510c20d4a6f819398475369f016c
Daniel Kim <dannyjwkim@lawn-143-215-88-90.lawn.gatech.edu>
2017-03-04 21:11:43 -0500

    Start Button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,63 +11,71 @@ var remainingQuestions = [
       question: 'The "Best Animated Feature Film" category debuted in 2001 and was won by:',
       answer: [ "Shrek", "Spirited Away", "Toy Story 2", "Monsters, Inc." ],
       correct: "Shrek",
-      image: "../images/shrek.gif"
+      image: "assets/images/shrek.gif"
     },
     {
       question: "The Moonlight/La La Land controversy puts to rest a false rumor that this actress'" + ' "win" was due to a presenter reading the wrong name:',
       answer: [ "Marisa Tomei", "Whoopi Goldberg", "Gwyneth Paltrow", "Renee Zellweger" ],
       correct: "Marisa Tomei",
-      image: "../images/tomei.gif"
+      image: "assets/images/tomei.gif"
     },
     {
       question: "Star Wars: The Force Awakens is the highest domestic grossing film of all time. What movie is the highest domestic grossing film of all time, but adjusted for inflation?",
       answer: [ "Gone With The Wind", "Star Wars: A New Hope", "The Sound of Music", "Titanic" ],
       correct: "Gone With The Wind",
-      image: "../images/wind.gif"
+      image: "assets/images/wind.gif"
     },
     {
       question: "Which actor has the most Oscar nominations with 12?",
       answer: [ "Al Pacino", "Jack Nicholson", "Marlon Brando", "Robert DeNiro" ],
       correct: "Jack Nicholson",
-      image: "../images/jack.gif"
+      image: "assets/images/jack.gif"
     },
     {
       question: "Which actress has the most Oscar wins with 4?",
       answer: [ "Jane Fonda", "Judi Dench", "Meryl Streep", "Katharine Hepburn" ],
       correct: "Katharine Hepburn",
-      image: "../images/hepburn.gif"
+      image: "assets/images/hepburn.gif"
     },
     {
       question: "All of these films tied the record for most Oscar nominations (14), except for:",
       answer: [ "La La Land", "All About Eve", "Titanic", "Forrest Gump" ],
       correct: "Forrest Gump",
-      image: "../images/gump.gif"
+      image: "assets/images/gump.gif"
     },
     {
       question: "All of these films tied the record for most Oscar wins (11), except for:",
       answer: [ "West Side Story", "Titanic", "Ben-Hur", "The Lord of the Rings: The Return of the King" ],
       correct: "West Side Story",
-      image: "../images/west.gif"
+      image: "assets/images/west.gif"
     }, 
     {
       question: "Who is the only person to win an Oscar for playing a real-life Oscar winner?",
       answer: [ "Paul Newman", "Meryl Streep", "Cate Blanchett", "Elizabeth Taylor" ],
       correct: "Cate Blanchett",
-      image: "../images/cate.gif"
+      image: "assets/images/cate.gif"
     },
     {
       question: "Which Foreign Language Film holds the record for most Oscar nominations with 10?",
       answer: [ "Life is Beautiful", "Pan's Labyrinth", "Amour", "Crouching Tiger, Hidden Dragon" ],
       correct: "Crouching Tiger, Hidden Dragon",
-      image: "../images/crouching.gif"
+      image: "assets/images/crouching.gif"
     },
     {
       question: "Who is the only African-American woman to win an Oscar for Best Actress in a Leading Role?",
       answer: [ "Octavia Spencer", "Viola Davis", "Jennifer Hudson", "Halle Berry" ],
       correct: "Halle Berry",
-      image: "../images/halle.gif"
+      image: "assets/images/halle.gif"
     },
   ];
+
+
+  $("button").on("click",function() {
+    $("#start").css("display", "none");
+    $("#asked-question").css("display", "block");
+    $("#choices").css("display", "block");
+    $("#timer").css("display", "block");
+  });
 
   function incrementTimer() {
     timer = setTimeout( function () {
@@ -114,7 +122,7 @@ var remainingQuestions = [
             console.log( "Correct!" );
             rightAnswers.push( questionObject );
             // $("#question-container").html("<img src ='" + "rightAnswers[0].image" + "'>");
-
+            
             askQuestion();
           } 
           else {
